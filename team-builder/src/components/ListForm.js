6f85c0cf9e047677796c210fb70c7ec64c2a28e4
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-02-26 16:29:03 -0500

    Complete ListForm component and render members

--- team-builder/src/components/ListForm.js
+++ team-builder/src/components/ListForm.js
@@ -7,18 +7,32 @@ const ListForm = props => {
         role: ""
     });
 
+    const handleChanges = event => {
+        setMember({...member, [event.target.name]: [event.target.value]})
+    }
+
+    const submitForm = event => {
+        event.preventDefault();
+        props.addNewMember(member);
+        setMember({name: "", email: "", role: ""});
+    };
+
     return (
-        <form>
-            <label>Name</label>
-            <input type = "text"></input>
+        <form onSubmit = {submitForm}>
+
+            <label htmlFor = "name">Name</label>
+            <input id = "name" name = "name" type = "text" onChange = {handleChanges} value = {member.name}></input>
             <br/>
-            <label>Email</label>
-            <input type = "text"></input>
+
+            <label htmlFor = "email">Email</label>
+            <input id = "email" name = "email" type = "email" onChange = {handleChanges} value = {member.email}></input>
             <br/>
-            <label>Role</label>
-            <input type = "text"></input>
+
+            <label htmlFor = "role">Role</label>
+            <input id = "role" name = "role" type = "text" onChange = {handleChanges} value = {member.role}></input>
             <br/>
-            <button>Add</button>
+
+            <button type = "submit">Add</button>
         </form>
     );
 };
